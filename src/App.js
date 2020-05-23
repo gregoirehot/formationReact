169e61fd33c79dd6b3e10589c86786110e77fe97
gregoire hot <gregoire.hot.partner@decathlon.com>
2020-05-23 14:25:44 +0200

    feat(welcome) step9

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ const clickHere = (value) => {
   console.log("clickHere", value);
 };
 
+const clickHere2 = (value) => {
+  console.log("clickHere2", value);
+};
+
+const clickHere3 = (value) => {
+  console.log("clickHere3", value);
+};
+
 function App() {
   return (
     <div className="App">
       <Welcome name="Greg" clickHere={clickHere} />
+      <Welcome name="Greg2" clickHere={clickHere2} />
+      <Welcome name="Greg3" clickHere={clickHere3} />
     </div>
   );
 }
